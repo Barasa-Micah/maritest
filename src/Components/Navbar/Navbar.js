059e3a43118859e10d9e507d1871b29a3addafff
username <barasamica@gmai.com>
2024-04-23 11:35:32 +0300

    Added assets

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Navbar.css';
+
+
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+        <img src={} alt='logo' className='logo'/>
+    </nav>
+  )
+}
+
+export default Navbar
+
