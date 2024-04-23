35838b4780061c2ea63457f6f3cbf4934090e21d
username <barasamica@gmai.com>
2024-04-23 15:54:22 +0300

    Styled Home

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import './Home.css';
-import Maritest from '../../assets/Maritest2-Photoroom.png-Photoroom.png/';
+import Maritest from '../../assets/Maritest2-Photoroom.png-Photoroom.png';
+import teaser from '../../assets/Mari Test teaser.pdf';
 const Home = () => {
     const handleDownloadTeaser = () => {
         const teaserPDF = require('../../assets/Mari Test teaser.pdf');
         const link = document.createElement('a')
         link.href = teaserPDF;
-        link.download = {resume};
+        link.download = {teaser};
         link.click();
     };
   return (
     <section id='intro'>
         <div className='introContent'>
-            <span>Mari Test, a device that will end Malaria  <br/> in a resource restrained communities</span>
+            <span className='introtext'>Mari Test, a device that will end Malaria  <br/> in a resource restrained communities</span>
             <button className='btn' onClick={handleDownloadTeaser}> Maritest Teaser</button>
         </div>
         <img  src={Maritest} alt='profile' className='bg'/>
