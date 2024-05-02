e819897e027c9c5a3df59bd403c929dca16d4b6f
karanidenis <d.waweru@alustudent.com>
2024-05-02 15:50:30 +0300

    Nav bar edits

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <Router>
       <div>
-        <Navbar />
+        {/* <Navbar /> */}
         <div className="main-content">
         <Routes>
           <Route path="/" element={<Home />} />
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './Home.css';
 import Maritest from '../../assets/Maritest2-Photoroom.png-Photoroom (1).png';
 import teaser from '../../assets/Mari Test teaser.pdf';
+import logo from '../../assets/Maritest1.jpg';
+
 
 const Home = () => {
   const handleDownloadTeaser = () => {
@@ -13,6 +15,8 @@ const Home = () => {
   };
 
   return (
+    <div className='home'>
+    <img src={logo} alt='logo' className='logo'/>
     <section id='intro'>
       <div className='introContent'>
         <span className='introtext'>Mari Test, a device that will end Malaria in resource-restrained communities</span>
@@ -20,6 +24,7 @@ const Home = () => {
       </div>
       <img src={Maritest} alt='profile' className='bg'/>
     </section>
+    </div>
   );
 }
 
